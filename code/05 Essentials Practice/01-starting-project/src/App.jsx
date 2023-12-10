cf5b4f7cdfbb7f1149837e8a83026c392ff18035
qingyangucb <qingyangucb@gmail.com>
2023-12-10 15:21:46 -0800

    work on 05

--- code/05 Essentials Practice/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,7 +1,23 @@
 import Header from "./components/Header";
+import { useState } from "react";
+import UserInput from "./components/UserInput";
 
+const STARTINGINPUT = {
+  initial: 10000,
+  annual: 1200,
+  Return: 6,
+  duration: 10,
+};
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState();
+  function handleInput() {
+    
+  }
+
+  return;
+  <main>
+    <Header />
+  </main>;
 }
 
 export default App;
